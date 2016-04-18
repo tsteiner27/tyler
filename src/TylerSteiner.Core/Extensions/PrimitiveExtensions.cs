4df3f6901d8b1f94b80243836a43261784f591cf
Ryan Erdmann <ryanerdmann@hotmail.com>
2016-04-18 17:47:54 -0500

    Update app UI

---
+++ src/TylerSteiner.Core/Extensions/PrimitiveExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace TylerSteiner.Core.Extensions
+{
+    public static class PrimitiveExtensions
+    {
+        public static string Truncate(this double number, int digits)
+        {
+            var str = number.ToString(CultureInfo.InvariantCulture);
+            var dot = str.IndexOf(".", StringComparison.InvariantCulture);
+            return str.Substring(0, dot + digits + 1);
+        }
+    }
+}
